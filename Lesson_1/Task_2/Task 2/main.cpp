8fbff078ed1c49cb47ed1e304284638f0e9aee7d
OmelchenkoStanislav <stasemelya2708@gmail.com>
2021-10-31 11:30:13 +0300

    added tasks 1, 2

---
+++ Lesson_1/Task_2/Task 2/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+cout <<"                            ,--.          "<< endl;
+cout <<"                           {    }"<< endl;
+cout <<"                           K,   }"<< endl;
+cout <<"                          /  `Y`"<< endl;
+cout <<"                     _   /   /"<< endl;
+cout <<"                    {_'-K.__/"<< endl;
+cout <<"                      `/-.__L._"<< endl;
+cout <<"                      /  ' /`\\_}"<< endl;
+cout <<"                     /  ' /"<< endl;
+cout <<"             ____   /  ' /"<< endl;
+cout <<"      ,-'~~~~    ~~/  ' /_"<< endl;
+cout <<"    ,'             ``~~~%%',"<< endl;
+cout <<"   (                    %  Y"<< endl;
+cout <<"  {                     %% I"<< endl;
+cout <<" {      -                %  `."<< endl;
+cout <<" |      ',                %  ) "<< endl;
+cout <<" |       |   ,..__       __. Y    "<< endl;
+cout <<" |   .,_./  Y  '/ ^Y   J   ) |    "<< endl;
+cout <<" \\          |' /   |   |   | |     "<< endl;
+cout <<"  \\         L_/    ._  (_,.' (      "<< endl;
+cout <<"   \\,   ,    ^^''' / |        )     "<< endl;
+cout <<"     \\_  \\        /,L]       /"<< endl;
+cout <<"       '-_`-,     ` `     ./`"<< endl;
+cout <<"          `-(_            )"<< endl;
+cout <<"              ^^\\..___,.--`"<< endl;
+cout <<""<< endl;
+
+
+
+
+
+    return 0;
+}
